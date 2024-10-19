bf77e201fa80844c2665ae688eeb2f36d680c00c
Kingshana0 <liyanaarachchikeshara2@gmail.com>
2024-10-19 20:02:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "lnNyRTaL#EX506wn6Zpgd02R8yFkLnojyEfu0jVadLQ0miiYzEag",
+SESSION_ID: process.env.SESSION_ID || "RqkzHARS#BGNLOY3dzKlR5Or3cG62U6hNsfs53spZcjqnXE-nGuM",
 MONGODB: process.env.MONGODB || "mongodb+srv://homodik802:G9le4VN9JGn5hcxQ@cluster0.auaczzj.mongodb.net/",
 };
